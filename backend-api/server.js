4d006b08820cc592f5f94c91a8f8016e76ec39d3
sethupk9733 <sethu9733@gmail.com>
2026-04-09 16:35:43 +0530

    fix: add marketplace to CORS origins

--- backend-api/server.js
+++ backend-api/server.js
@@ -19,6 +19,7 @@ app.use(cors({
         'https://www.gymkaana.com',
         'https://owner.gymkaana.com',
         'https://admin.gymkaana.com',
+        'https://app.gymkaana.com',
         'http://localhost:5173',
         'http://localhost:5174',
         'http://localhost:3000',
